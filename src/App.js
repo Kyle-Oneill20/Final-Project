ac94aa7c79e08500bb5274d0a64fdcd1b21017db
Kyle-Oneill20 <142435616+Kyle-Oneill20@users.noreply.github.com>
2023-11-27 19:59:38 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
+import Home from './Components/Home.js'
+import About from './Components/About.js'
+import Page from './Components/page3.js'
+
+
+export default function App() {
+  return (
+<Router>
+   <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/page3">Page</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/page3">
+            <Page/>
+          </Route>
+
+          <Route path="/about">
+            <About />
+          </Route>
+
+          <Route path="/home">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+
+
+

69dfeb762ff610ac0b56ad954e54cabfb24767b4
Kyle-Oneill20 <142435616+Kyle-Oneill20@users.noreply.github.com>
2023-12-05 14:17:33 -0500

    Delete src/Components directory

--- src/Components/Link.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-
-export default function Redirect(){
-    return(
-       <a href="./Components\Update.js"><button className="redirect btn btn-primary "onClick={"target"}>Update Info</button></a>
-    )
-}

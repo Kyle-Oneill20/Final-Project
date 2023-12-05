7e1a9adf973c64339ba8c851701bb9a6c6403db6
Kyle-Oneill20 <142435616+Kyle-Oneill20@users.noreply.github.com>
2023-12-05 14:17:47 -0500

    Delete src/components directory

--- src/components/page3.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Page() {
-	
-		return <div> 
-            <h2 text="text-uppercase, text-center">Check Out to take your pet home</h2>;
-	
-    </div>
-}

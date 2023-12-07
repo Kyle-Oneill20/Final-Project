f1217fba84b44cb2077b0aabf360343c94324717
Kyle-Oneill20 <142435616+Kyle-Oneill20@users.noreply.github.com>
2023-12-07 12:39:57 -0500

    Update Update.js

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -33,7 +33,7 @@ export default function Update(){
             headers:{
                 "Content-Type": "application/json"
             },
-        }).then(console.log(updatedPetObject)).then(()=>getPets()).then(alert("Pet Info Updated!")).then(Update)
+        }).then(console.log(updatedPetObject)).then(()=>getPets()).then(alert("Pet Info Updated!")).then(Home)
     }
     
     
